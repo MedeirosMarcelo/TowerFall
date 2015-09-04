57e4201a369cef99289ddb46718aca9d4631efde
MedeirosMarcelo <15191964@W1801307.portoalegre.pucrsnet.br>
2015-09-03 21:08:02 -0300

    !!

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -11,7 +11,7 @@ public class Controller : MonoBehaviour {
 
 	GroundCollider groundCollider;
 	float runSpeed = 5f;
-	float jumpForce = 18f;
+	float jumpForce = 30f;
 	Vector3 direction;
 	State state;
 
@@ -38,17 +38,15 @@ public class Controller : MonoBehaviour {
 	}
 
 	void Move() {
-
-		direction = new Vector3(Input.GetAxis ("Horizontal"), 0, Input.GetAxis("Vertical"));
-
+		direction = new Vector3(Input.GetAxis ("Horizontal") * runSpeed, 0, Input.GetAxis("Vertical") * runSpeed);
 		if (isGrounded){
 			if (Input.GetButtonDown ("Jump")) {
 				Debug.Log("JUMP");
 				rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpForce, rigidbody.velocity.z);
 			}
 		}
-		Vector3 velocity = new Vector3 (direction.x * runSpeed, direction.y * jumpForce, direction.z * runSpeed);
-		rigidbody.MovePosition (this.transform.position + velocity * Time.deltaTime);
+		Test ();
+		rigidbody.MovePosition (this.transform.position + direction * Time.deltaTime);
 	}
 
 	void Shoot(){
@@ -64,6 +62,26 @@ public class Controller : MonoBehaviour {
 		Vector3 arrowSpawner = transform.Find ("ArrowSpawner").position;
 		GameObject newArrow = (GameObject)Instantiate (arrow, arrowPosition, arrowRotation);
 		newArrow.GetComponent<Arrow>().shot = true;
+	}
+	
+	void Test()
+	{
+		// Get the velocity
+		Vector3 horizontalMove = rigidbody.velocity;
+		// Don't use the vertical velocity
+		horizontalMove.y = 0;
+		// Calculate the approximate distance that will be traversed
+		float distance =  horizontalMove.magnitude * Time.fixedDeltaTime;
+		// Normalize horizontalMove since it should be used to indicate direction
+		horizontalMove.Normalize();
+		RaycastHit hit;
+		
+		// Check if the body's current velocity will result in a collision
+		if(rigidbody.SweepTest(horizontalMove, out hit, distance))
+		{
+			// If so, stop the movement
+			rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0);
+		}
 	}
 }
 
