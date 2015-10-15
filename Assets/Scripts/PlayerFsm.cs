355622b00c1bea43da72b1a146cb63995ea19127
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2015-10-15 20:20:22 -0300

    Scene Backup

---
+++ Assets/Scripts/PlayerFsm.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFsm : MonoBehaviour {
+
+    State state;
+
+    enum State {
+        Idle,
+        Run,
+        Shoot,
+        Jump,
+        Crouch,
+        Dash,
+        GrabLedge,
+        Climb,
+        SlideDown,
+        Fall,
+        Die
+    }
+
+    void Update() {
+        switch (state) {
+            case State.Idle:
+                break;
+            case State.Run:
+                break;
+            case State.Shoot:
+                break;
+            case State.Jump:
+                break;
+            case State.Crouch:
+                break;
+            case State.Dash:
+                break;
+            case State.GrabLedge:
+                break;
+            case State.Climb:
+                break;
+            case State.SlideDown:
+                break;
+            case State.Fall:
+                break;
+            case State.Die:
+                break;
+        }
+    }
+
+    void EnterState(State newState) {
+        ExitState();
+        state = newState;
+
+        switch (state) {
+            case State.Idle:
+                break;
+            case State.Run:
+                break;
+            case State.Shoot:
+                break;
+            case State.Jump:
+                break;
+            case State.Crouch:
+                break;
+            case State.Dash:
+                break;
+            case State.GrabLedge:
+                break;
+            case State.Climb:
+                break;
+            case State.SlideDown:
+                break;
+            case State.Fall:
+                break;
+            case State.Die:
+                break;
+        }
+    }
+
+    void ExitState() {
+        switch (state) {
+            case State.Idle:
+                break;
+            case State.Run:
+                break;
+            case State.Shoot:
+                break;
+            case State.Jump:
+                break;
+            case State.Crouch:
+                break;
+            case State.Dash:
+                break;
+            case State.GrabLedge:
+                break;
+            case State.Climb:
+                break;
+            case State.SlideDown:
+                break;
+            case State.Fall:
+                break;
+            case State.Die:
+                break;
+        }
+    }
+
+
+}
