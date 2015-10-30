a8067596699a7c3b645b85943ec57db51a46edf1
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2015-10-30 20:44:57 -0200

    Added really basic server scene

---
+++ Assets/Scripts/network/Client.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Client : MonoBehaviour {
+    /*
+    public InputField ipInput;
+    public InputField portInput;
+
+    public Text PrefabText;
+
+    public Button connectionButton { get; private set; }
+    public InputFieldManager nameInput { get; private set; }
+    public InputFieldManager commandInput { get; private set; }
+    public ScrollRect scroolRect { get; private set; }
+    bool updateScrool;
+
+    string playerName;
+    NetworkView net;
+
+    void Start() {
+        net = GetComponent<NetworkView>();
+        connectionButton = GameObject.FindGameObjectWithTag("Connection").GetComponent<Button>();
+        nameInput = GameObject.FindGameObjectWithTag("Name").GetComponent<InputFieldManager>();
+        commandInput = GameObject.FindGameObjectWithTag("Command").GetComponent<InputFieldManager>();
+        scroolRect = GameObject.FindGameObjectWithTag("Scrool").GetComponent<ScrollRect>();
+
+        scroolRect.onValueChanged.AddListener(val => {
+            if (updateScrool) {
+                Debug.Log("Update Scrool");
+                scroolRect.verticalNormalizedPosition = 0;
+                scroolRect.verticalScrollbar.value = 0;
+                updateScrool = false;
+            }
+        });
+
+        commandInput.onSubmit += delegate () {
+            if (Network.peerType != NetworkPeerType.Disconnected) {
+                net.RPC("Command", RPCMode.Server, commandInput.text);
+            }
+        };
+        nameInput.onSubmit += delegate () {
+            if (Network.peerType == NetworkPeerType.Disconnected) {
+                Connect();
+            }
+        };
+
+        connectionButton.onClick.AddListener(() => {
+            if (Network.peerType == NetworkPeerType.Disconnected) {
+                Connect();
+            }
+            else {
+                Disconnect();
+            }
+        });
+    }
+
+    bool IsValidName(string name) {
+        return (name.Length > 2);
+    }
+
+    public void Connect() {
+        if (IsValidName(nameInput.text)) {
+            playerName = nameInput.text;
+            int port;
+            if (!int.TryParse(portInput.text, out port)) {
+                port = 25001;
+            }
+            string ip = (ipInput.text == string.Empty) ? "127.0.0.1" : ipInput.text;
+            Debug.Log(ip + ":" + port);
+            Network.Connect(ip, port);
+            Debug.Log("Connect");
+        }
+    }
+    public void Disconnect() {
+        Network.Disconnect(250);
+        Debug.Log("Disconnect");
+    }
+
+    void OnConnectedToServer() {
+        Debug.Log("Connected to server");
+        net.RPC("Login", RPCMode.Server, playerName);
+        connectionButton.GetComponentInChildren<Text>().text = "Logout";
+        nameInput.interactable = false;
+        commandInput.interactable = true;
+
+        EventSystem.current.SetSelectedGameObject(commandInput.gameObject);
+    }
+    void OnDisconnectedFromServer() {
+        Debug.Log("Disconnected");
+        connectionButton.GetComponentInChildren<Text>().text = "Login";
+        nameInput.interactable = true;
+        commandInput.interactable = false;
+    }
+
+    [RPC]
+    void AddText(string text) {
+        Debug.Log("AddText: " + text);
+        var newText = Instantiate(PrefabText) as Text;
+        newText.text = text;
+        newText.rectTransform.SetParent(scroolRect.content.transform);
+        updateScrool = true;
+    }
+
+    [RPC]
+    void Login(string playerName, NetworkMessageInfo info) {
+    }
+
+    [RPC]
+    void Command(string command, NetworkMessageInfo info) {
+    }
+    */
+}
---
+++ Assets/Scripts/network/Server.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Server : MonoBehaviour {
+
+    public Button startButton { get; private set; }
+    public InputField portInput { get; private set; }
+    public Text statusText { get; private set; }
+
+
+    string playerName;
+    NetworkView net;
+
+    void Start() {
+
+        net = GetComponent<NetworkView>();
+        startButton = GameObject.FindGameObjectWithTag("Server Start").GetComponent<Button>();
+        portInput = GameObject.FindGameObjectWithTag("Server Port").GetComponent<InputField>();
+        statusText = GameObject.FindGameObjectWithTag("Server Status").GetComponent<Text>();
+
+        startButton.onClick.AddListener(() => {
+            if (Network.peerType == NetworkPeerType.Disconnected) {
+                int port;
+                if (!int.TryParse(portInput.text, out port)) {
+                    port = 25001;
+                }
+                Network.InitializeServer(10, port);
+            }
+            else {
+                Network.Disconnect(250);
+            }
+        });
+    }
+
+    void OnServerInitialized() {
+            Debug.Log("OnServerInitialized");
+            startButton.GetComponentInChildren<Text>().text = "Stop Server";
+            statusText.text = "Server online\nConnections: " + Network.connections.Length;
+        }
+
+        void OnDisconnectedFromServer() {
+            Debug.Log("OnDisconnectedFromServer");
+            startButton.GetComponentInChildren<Text>().text = "Start Server";
+            statusText.text = "Server offline";
+        }
+
+        void OnPlayerConnected(NetworkPlayer player) {
+            Debug.Log("OnPlayerConnected");
+            statusText.text = "Server online\nConnections: " + Network.connections.Length;
+        }
+
+        void OnPlayerDisconnected(NetworkPlayer player) {
+            Debug.Log("OnPlayerDisconnected");
+            statusText.text = "Server online\nConnections: " + (Network.connections.Length - 1);
+        }
+
+
+    [RPC]
+    void Login(string playerName, NetworkMessageInfo info) {
+    }
+}
