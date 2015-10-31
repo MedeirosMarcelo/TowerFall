7511d39b394a66f8aa8d7bdb19d76258f264d00f
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2015-10-30 22:29:06 -0200

    Client Scene
    Elas se conectam OMG!

--- Assets/Scripts/network/Client.cs
+++ Assets/Scripts/network/Client.cs
@@ -5,49 +5,22 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Client : MonoBehaviour {
-    /*
-    public InputField ipInput;
-    public InputField portInput;
 
-    public Text PrefabText;
+    public Button startButton { get; private set; }
+    public InputField ipInput { get; private set; }
+    public InputField portInput { get; private set; }
+    public InputField nameInput { get; private set; }
 
-    public Button connectionButton { get; private set; }
-    public InputFieldManager nameInput { get; private set; }
-    public InputFieldManager commandInput { get; private set; }
-    public ScrollRect scroolRect { get; private set; }
-    bool updateScrool;
-
-    string playerName;
     NetworkView net;
 
     void Start() {
         net = GetComponent<NetworkView>();
-        connectionButton = GameObject.FindGameObjectWithTag("Connection").GetComponent<Button>();
-        nameInput = GameObject.FindGameObjectWithTag("Name").GetComponent<InputFieldManager>();
-        commandInput = GameObject.FindGameObjectWithTag("Command").GetComponent<InputFieldManager>();
-        scroolRect = GameObject.FindGameObjectWithTag("Scrool").GetComponent<ScrollRect>();
+        startButton = GameObject.FindGameObjectWithTag("Client Start").GetComponent<Button>();
+        ipInput = GameObject.FindGameObjectWithTag("Client IP").GetComponent<InputField>();
+        portInput = GameObject.FindGameObjectWithTag("Client Port").GetComponent<InputField>();
+        nameInput = GameObject.FindGameObjectWithTag("Client Name").GetComponent<InputField>();
 
-        scroolRect.onValueChanged.AddListener(val => {
-            if (updateScrool) {
-                Debug.Log("Update Scrool");
-                scroolRect.verticalNormalizedPosition = 0;
-                scroolRect.verticalScrollbar.value = 0;
-                updateScrool = false;
-            }
-        });
-
-        commandInput.onSubmit += delegate () {
-            if (Network.peerType != NetworkPeerType.Disconnected) {
-                net.RPC("Command", RPCMode.Server, commandInput.text);
-            }
-        };
-        nameInput.onSubmit += delegate () {
-            if (Network.peerType == NetworkPeerType.Disconnected) {
-                Connect();
-            }
-        };
-
-        connectionButton.onClick.AddListener(() => {
+        startButton.onClick.AddListener(() => {
             if (Network.peerType == NetworkPeerType.Disconnected) {
                 Connect();
             }
@@ -63,17 +36,17 @@ public class Client : MonoBehaviour {
 
     public void Connect() {
         if (IsValidName(nameInput.text)) {
-            playerName = nameInput.text;
+            //playerName = nameInput.text;
             int port;
             if (!int.TryParse(portInput.text, out port)) {
                 port = 25001;
             }
             string ip = (ipInput.text == string.Empty) ? "127.0.0.1" : ipInput.text;
-            Debug.Log(ip + ":" + port);
             Network.Connect(ip, port);
             Debug.Log("Connect");
         }
     }
+
     public void Disconnect() {
         Network.Disconnect(250);
         Debug.Log("Disconnect");
@@ -81,35 +54,18 @@ public class Client : MonoBehaviour {
 
     void OnConnectedToServer() {
         Debug.Log("Connected to server");
-        net.RPC("Login", RPCMode.Server, playerName);
-        connectionButton.GetComponentInChildren<Text>().text = "Logout";
+        //net.RPC("Login", RPCMode.Server, playerName);
+        startButton.GetComponentInChildren<Text>().text = "Logout";
         nameInput.interactable = false;
-        commandInput.interactable = true;
-
-        EventSystem.current.SetSelectedGameObject(commandInput.gameObject);
     }
+
     void OnDisconnectedFromServer() {
         Debug.Log("Disconnected");
-        connectionButton.GetComponentInChildren<Text>().text = "Login";
+        startButton.GetComponentInChildren<Text>().text = "Login";
         nameInput.interactable = true;
-        commandInput.interactable = false;
-    }
-
-    [RPC]
-    void AddText(string text) {
-        Debug.Log("AddText: " + text);
-        var newText = Instantiate(PrefabText) as Text;
-        newText.text = text;
-        newText.rectTransform.SetParent(scroolRect.content.transform);
-        updateScrool = true;
     }
 
     [RPC]
     void Login(string playerName, NetworkMessageInfo info) {
     }
-
-    [RPC]
-    void Command(string command, NetworkMessageInfo info) {
-    }
-    */
 }
