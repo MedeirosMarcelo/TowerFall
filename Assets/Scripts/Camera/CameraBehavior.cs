d9801684d59234e82a0031ea31257529128da013
MedeirosMarcelo <15191964@W1801307.portoalegre.pucrsnet.br>
2015-11-04 22:34:32 -0200

    Camera first try

--- Assets/Scripts/Camera/CameraBehavior.cs
+++ Assets/Scripts/Camera/CameraBehavior.cs
@@ -3,17 +3,29 @@ using System.Collections;
 
 public class CameraBehavior : MonoBehaviour {
 
-    public float rayDistance = 25f;
+    public GameObject owner;
+    public float speed = 2f;
+    public float maxDistance = -5.5f;
+    public float rayDistance = 5f;
+    bool adjustPosition;
 
     void Start() {
-        
+
     }
 
-    bool DetectFront() {
+    void Update() {
+        AdjustPosition();
+    }
+
+    bool SightBlocked() {
+        Vector3 direction = transform.parent.position - transform.position;
         RaycastHit hit;
-        Ray ray = new Ray(this.transform.position, Vector3.forward);
+        Ray ray = new Ray(this.transform.position, direction);
+        Debug.DrawRay(this.transform.position, direction, Color.blue);
         if (Physics.Raycast(ray, out hit, rayDistance)) {
+            Debug.Log(hit.collider.name);
             if (hit.collider.name == "Wall") {
+                Debug.Log("SightBlocked");
                 return true;
             }
         }
@@ -22,13 +34,21 @@ public class CameraBehavior : MonoBehaviour {
 
     void AdjustPosition() {
 
-        Vector3 origin = Vector3.zero;
-        Vector3 destination = Vector3.zero;
-        float val = 0f;
-        transform.position = Vector3.Lerp(origin, destination, val);
+        Vector3 origin = this.transform.localPosition;
+        Vector3 positionNear = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+        Vector3 positionFar = new Vector3(transform.localPosition.x, transform.localPosition.y, maxDistance);
+
+        if (SightBlocked()) {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, Mathf.Lerp(transform.localPosition.z, 0, speed * Time.deltaTime));
+        //    transform.localPosition = Vector3.Lerp(origin, positionNear, speed);
+        }
+        else {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, Mathf.Lerp(transform.localPosition.z, maxDistance, speed * Time.deltaTime));
+        //    transform.localPosition = Vector3.Lerp(origin, positionFar, speed);
+        }
     }
 
-    void OnTriggerEnter(Collider col) {
-        
+    void OnTriggerExit(Collider col) {
+
     }
 }
