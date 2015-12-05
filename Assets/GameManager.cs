928bb55a3d46a25bcb2c5674267d5930faa39f30
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2015-12-05 16:03:16 -0200

    Basic Menu Scene

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager gameManager = null;
+ 
+	void Start () {
+        if(gameManager != null){
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+	}
+	
+    void OnDisconnectedFromServer() {
+        Debug.Log("OnDisconnected");
+    }
+
+
+}
